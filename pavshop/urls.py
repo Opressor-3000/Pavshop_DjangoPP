cefa91d4ac915e2daa2e36e41d5027cc34a6c8a2
gyulievyusif <yannis1@rambler.ru>
2023-07-29 20:52:23 +0400

    add paginator to product list template

--- pavshop/urls.py
+++ pavshop/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls.static import static
-# from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('account/', include('account.urls'), name='account'),
     path('blog/', include('blog.urls'), name='bloglist'),
     path('product/', include('product.urls'), name='product'),
